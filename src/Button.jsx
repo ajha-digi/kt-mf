133c73458e3de42e6a8d970370c63661e8e87607
ajha-digi <ajha@digisprint.com>
2024-11-26 10:54:45 +0530

    implemeted mf app for remote

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>Button from remote App</div>
+  )
+}
+
+export default Button
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "demo_remote_app",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./demoApp" : "./src/App.jsx",
+        "./button" : "./src/Button.jsx"
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
